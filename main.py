f889a2c0f5b6fb9427fa434f2bf69af7b295b92f
farmalifegood-del <farmalifegood@gmail.com>
2026-09-18 21:26:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from groq import Groq
 
 logging.basicConfig(level=logging.INFO)
 
-BOT_TOKEN = "8588322130:AAHiAxJNOscxuS-bA3EnYfCgO21K3SAAUaw"
+BOT_TOKEN = "8588322130:AAEMlT8OdWO1UeyKdPgAVzV2-wKyW6ndQME"
 # Shu yerga gsk_... bilan boshlanadigan Groq kalitingizni joylang:
 GROQ_API_KEY = "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" 
 
